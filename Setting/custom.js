e3245811b425ff33a09d9d35e23ad033d2449936
Đức Hiếu <chodenmai@gmail.com>
2021-03-30 18:52:07 +0700

    init

--- Setting/custom.js
+++ Setting/custom.js
@@ -1,10 +1,27 @@
+var isSet = false;
+
 var setting_ava = document.getElementById("setting-avatar");
 var custom_ava = document.getElementById("custom-ava");
 custom_ava.style.display = "none";
-var isSet = false;
-
+var ava = document.getElementById("ava");
 setting_ava.addEventListener('click', function () {
     isSet = !isSet;
     isSet ? custom_ava.style.display = "block" : custom_ava.style.display = "none";
     console.log(isSet);
+    isSet ? ava.style.display = "none" : ava.style.display = "block";
 });
+
+var setting_name = document.getElementById("setting-name");
+var custom_name = document.getElementById("custom-name");
+custom_name.style.display = "none";
+var username = document.getElementById("username");
+setting_name.addEventListener('click', function () {
+    isSet = !isSet;
+    isSet ? custom_name.style.display = "block" : custom_name.style.display = "none";
+    console.log(isSet);
+    isSet ? username.style.display = "none" : username.style.display = "block";
+});
+
+
+
+
