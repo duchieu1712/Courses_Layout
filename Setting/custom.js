c33fcb440257f7575c10c90cc85134358e865d1d
thang1509 <lnthang1509@gmail.com>
2021-03-30 20:16:12 +0700

    init

--- Setting/custom.js
+++ Setting/custom.js
@@ -11,6 +11,7 @@ setting_ava.addEventListener('click', function () {
     isSet ? ava.style.display = "none" : ava.style.display = "block";
 });
 
+var isSet1 =false
 var setting_name = document.getElementById("setting-name");
 var custom_name = document.getElementById("custom-name");
 custom_name.style.display = "none";
@@ -22,6 +23,27 @@ setting_name.addEventListener('click', function () {
     isSet ? username.style.display = "none" : username.style.display = "block";
 });
 
+var inFo = document.getElementById("info");
+var custom_info = document.getElementById("custom-info");
+custom_info.style.display = "none";
+var setting_info = document.getElementById("setting-info");
+setting_info.addEventListener('click', function () {
+    isSet1 = !isSet1;
+    isSet1 ? custom_info.style.display = "block" : custom_info.style.display = "none";
+    console.log(isSet);
+    isSet1 ? inFo.style.display = "none" : inFo.style.display = "table";
+});
 
+var isSet2 = false;
+var pass_current = document.getElementById("pass-current");
+var pasS = document.getElementById("pass");
+pasS.style.display = "none";
+var edit_ = document.getElementById("edit");
+edit_.addEventListener('click', function () {
+    isSet2 = !isSet2;
+    isSet2 ? pasS.style.display = "block" : pasS.style.display = "none";
+    console.log(isSet2);
+    isSet2 ? pass_current.style.display = "none" : pass_current.style.display = "table";
+});
 
 
